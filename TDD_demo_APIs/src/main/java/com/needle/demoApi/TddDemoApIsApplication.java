949887bbe355d04599611875c685ff7212d35d08
Ra8521 <taraon71@gmail.com>
2021-05-14 20:10:40 +0530

    add

---
+++ TDD_demo_APIs/src/main/java/com/needle/demoApi/TddDemoApIsApplication.java
@@ -0,0 +1,13 @@
+package com.needle.demoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddDemoApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddDemoApIsApplication.class, args);
+	}
+
+}
---
+++ TDD_demo_APIs/src/test/java/com/needle/demoApi/TddDemoApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.needle.demoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddDemoApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
